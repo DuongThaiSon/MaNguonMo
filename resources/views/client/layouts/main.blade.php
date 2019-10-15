b5b19f7c87f396fba0ccde3ebaee54555e84a36c
DuongThaiSon <sontd@leotive.com>
2019-10-15 11:22:26 +0700

    gan lien ket

--- resources/views/client/layouts/main.blade.php
+++ resources/views/client/layouts/main.blade.php
@@ -4,50 +4,52 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
+    <title>Trang chủ - @yield('title')</title>
     <link href="https://fonts.googleapis.com/css?family=Poppins:300,400,500,600,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,700" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Great+Vibes" rel="stylesheet">
 
-    <link rel="stylesheet" href="css/open-iconic-bootstrap.min.css">
-    <link rel="stylesheet" href="css/animate.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/open-iconic-bootstrap.min.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/animate.css">
 
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <link rel="stylesheet" href="css/owl.theme.default.min.css">
-    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/owl.theme.default.min.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/magnific-popup.css">
 
-    <link rel="stylesheet" href="css/aos.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/aos.css">
 
-    <link rel="stylesheet" href="css/ionicons.min.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/ionicons.min.css">
 
-    <link rel="stylesheet" href="css/bootstrap-datepicker.css">
-    <link rel="stylesheet" href="css/jquery.timepicker.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/bootstrap-datepicker.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/jquery.timepicker.css">
 
 
-    <link rel="stylesheet" href="css/flaticon.css">
-    <link rel="stylesheet" href="css/icomoon.css">
-    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/flaticon.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/icomoon.css">
+    <link rel="stylesheet" href="{{ asset('client/asset') }}/css/style.css">
 </head>
-
+@include('client.partials.header')
+@yield('content')
+@include('client.partials.footer')
 <body>
-    <script src="js/jquery.min.js"></script>
-    <script src="js/jquery-migrate-3.0.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/jquery.easing.1.3.js"></script>
-    <script src="js/jquery.waypoints.min.js"></script>
-    <script src="js/jquery.stellar.min.js"></script>
-    <script src="js/owl.carousel.min.js"></script>
-    <script src="js/jquery.magnific-popup.min.js"></script>
-    <script src="js/aos.js"></script>
-    <script src="js/jquery.animateNumber.min.js"></script>
-    <script src="js/bootstrap-datepicker.js"></script>
-    <script src="js/jquery.timepicker.min.js"></script>
-    <script src="js/scrollax.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery-migrate-3.0.1.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/popper.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/bootstrap.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.easing.1.3.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.waypoints.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.stellar.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/owl.carousel.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.magnific-popup.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/aos.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.animateNumber.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/bootstrap-datepicker.js"></script>
+    <script src="{{ asset('client/asset') }}/js/jquery.timepicker.min.js"></script>
+    <script src="{{ asset('client/asset') }}/js/scrollax.min.js"></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBVWaKrjvy3MaE7SQ74_uJiULgl1JY0H2s&sensor=false">
     </script>
-    <script src="js/google-map.js"></script>
-    <script src="js/main.js"></script>
+    <script src="{{ asset('client/asset') }}/js/google-map.js"></script>
+    <script src="{{ asset('client/asset') }}/js/main.js"></script>
 </body>
 
 </html>
