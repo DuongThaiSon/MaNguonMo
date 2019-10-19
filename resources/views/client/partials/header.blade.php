8c33a9a85a7432b462c49578da75cfd75bf2da6e
DuongThaiSon <sontd@leotive.com>
2019-10-19 11:01:27 +0700

    fix-menu

--- resources/views/client/partials/header.blade.php
+++ resources/views/client/partials/header.blade.php
@@ -7,27 +7,27 @@
         </button>
         <div class="collapse navbar-collapse" id="ftco-nav">
             <ul class="navbar-nav ml-auto">
-                <li class="nav-item active">
+                <li class="nav-item {{ Request::is('/') ? 'active' : '' }}">
                     <a href="/" class="nav-link">Home</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('menu') ? 'active' : '' }}">
                     <a href="menu" class="nav-link">Menu</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('service') ? 'active' : '' }}">
                     <a href="service" class="nav-link">Services</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('about') ? 'active' : '' }}">
                     <a href="about" class="nav-link">About</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('shop') ? 'active' : '' }}">
                     <a href="shop" class="nav-link">Shop</a>
                 </li>
-                <li class="nav-item">
+                <li class="nav-item {{ Request::is('contact') ? 'active' : '' }}">
                     <a href="contact" class="nav-link">Contact</a>
                 </li>
                 <li class="nav-item cart">
                     <a href="cart" class="nav-link">
-                        <span class="icon icon-shopping_cart"></span>
+                        <span class="icon icon-shopping_cart {{ Request::is('cart') ? 'active-cart' : '' }}"></span>
                         <span class="bag d-flex justify-content-center align-items-center">
                             <small>1</small>
                         </span>
