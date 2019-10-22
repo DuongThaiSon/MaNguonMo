e3cb78ebcf0a0dd5292a285995b5e1eb623202b3
DuongThaiSon <sontd@leotive.com>
2019-10-22 15:01:41 +0700

    add thư viện admin

--- public/assets/admin/asset/js/script.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-    // if ($(window).scrollTop() >= 81) {
-    //     $('.header').addClass('fixed-menu');
-    // } else {
-    //     $('.header').removeClass('fixed-menu');
-    // }
-});
