ba88059c1f615efd1693de2c71837bd2ce46ac88
DuongThaiSon <sontd@leotive.com>
2019-10-24 15:34:40 +0700

    fix logo header

--- resources/views/client/partials/header.blade.php
+++ resources/views/client/partials/header.blade.php
@@ -1,7 +1,11 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
         <a class="navbar-brand" href="/">
-            <img src="{{ asset('assets/admin') }}/images/logo2.PNG" alt="">
+            <img src="{{ asset('assets/client') }}/images/logo3.png" alt="">
+            <div class="float-right logo-text">
+                <span>Coffee</span>
+                <small>Blend</small>
+            </div>
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav"
             aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
