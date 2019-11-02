b5e028be29af2b9ede952ad9a5f68e180d6e1313
DuongThaiSon <sontd@leotive.com>
2019-11-02 18:13:05 +0700

    thêm giỏ hàng

---
+++ resources/views/admin/views/cart/cart-abort.blade.php
@@ -0,0 +1,84 @@
+@extends('admin.layouts.main')
+@section('title','Giỏ hàng')
+@section('content')
+<div class="main-card mb-3 card">
+        <div class="card-body">
+            <table class="mb-0 table table-hover">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Trạng thái</th>
+                        <th>Người bán</th>
+                        <th>Người nhận</th>
+                        <th>Số điện thoại</th>
+                        <th>Địa chỉ</th>
+                        <th>Email</th>
+                        <th>Discount</th>
+                        <th>Tổng tiền</th>
+                        <th>Thời gian giao</th>
+                        <th>Thời gian nhận</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>1</td>
+                        <td>Đã hủy</td>
+                        <td>Hà Văn Tú</td>
+                        <td>Dương Thái Sơn</td>
+                        <td>0332214203</td>
+                        <td>Số 61 Văn Cao</td>
+                        <td>duongthaison98@gmail.com</td>
+                        <td>0</td>
+                        <td>20000</td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Đã hủy</td>
+                        <td>Hà Văn Tú</td>
+                        <td>Dương Thái Sơn</td>
+                        <td>0332214203</td>
+                        <td>Số 61 Văn Cao</td>
+                        <td>duongthaison98@gmail.com</td>
+                        <td>0</td>
+                        <td>20000</td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>1</td>
+                        <td>Đã hủy</td>
+                        <td>Hà Văn Tú</td>
+                        <td>Dương Thái Sơn</td>
+                        <td>0332214203</td>
+                        <td>Số 61 Văn Cao</td>
+                        <td>duongthaison98@gmail.com</td>
+                        <td>0</td>
+                        <td>20000</td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                        <td>
+                            <span>20:00</span>
+                            <span>20/10/2019</span>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
